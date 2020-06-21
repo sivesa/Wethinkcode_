3b7e3c043abe392c5deb04f299be3bfaa07e53b1
Sive Sandla <sisandla@c4r8s10.wethinkcode.co.za>
2020-06-21 10:44:41 +0200

    new file ft_putnbr.c

---
+++ bootcamp/d02/ex06/ft_putnbr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sisandla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 10:43:44 by sisandla          #+#    #+#             */
+/*   Updated: 2020/06/21 10:44:06 by sisandla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		nb *= -1;
+		ft_putchar('-');
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+int main(void)
+{
+	ft_putnbr(-91);
+	ft_putchar('\n');
+	return 0;
+}
